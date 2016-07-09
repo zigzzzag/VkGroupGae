245aeb584ea5c2b277a5cced9deb0d0b4dd2b66d
zigzzzag <maximnikiforov1991@gmail.com>
2016-07-09 23:11:01 +0600

    add isEmptyOrWhiteSpace test

---
+++ src/test/StringUtilTest.java
@@ -0,0 +1,22 @@
+import com.google.appengine.repackaged.com.google.common.base.StringUtil;
+import junit.framework.TestCase;
+
+/**
+ * Created by Zigzag on 09.07.2016.
+ */
+public class StringUtilTest extends TestCase {
+
+    public void testIsEmptyOrWhitespace() {
+        String s1 = "";
+        String s2 = null;
+        String s3 = " ";
+        String s4 = "   ";//tab
+        String s5 = "Hello";
+
+        assertEquals(true, StringUtil.isEmptyOrWhitespace(s1));
+        assertEquals(true, StringUtil.isEmptyOrWhitespace(s2));
+        assertEquals(true, StringUtil.isEmptyOrWhitespace(s3));
+        assertEquals(true, StringUtil.isEmptyOrWhitespace(s4));
+        assertEquals(false, StringUtil.isEmptyOrWhitespace(s5));
+    }
+}
